43132e57ce8772ffa24e60597c70534a48290ee1
Adria Ros Saez <adria.ros.saez@everis.com>
2018-01-09 13:01:14 +0100

    Project Structure Initialization

---
+++ CleanArchExample/CleanArchExample/Scenes/ViewWeather/ViewWeatherRouter.swift
@@ -0,0 +1,11 @@
+//
+//  ViewWeatherRouter.swift
+//  CleanArchExample
+//
+//  Created by Adria Ros Saez on 1/9/18.
+//  Copyright © 2018 adriaros. All rights reserved.
+//
+
+import Foundation
+
+class ViewWeatherRouter{}
